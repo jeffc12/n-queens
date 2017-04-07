08ec27809e4217c742e35b04f8c3ee0b31009a08
Hack Reactor Student <student@hackreactor.com>
2017-04-06 20:07:22 -0700

    COMPLETED BASIC REQ

--- src/solvers.js
+++ src/solvers.js
@@ -13,36 +13,52 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 window.findNRooksSolution = function(n) {
-  // var solution = undefined; //fixme
+
   var solution = new Board({n: n});
-  // var newBoard = new Board({n: 3});
-  solution.togglePiece(0, 0);
 
   for (var i = 0; i < n; i++) {
     for (var j = 0; j < n; j++) {
-      if (solution.rows()[i][j] === 1) {
-        continue;
-      } else {
-        solution.togglePiece(i, j)
-        if (solution.hasAnyRooksConflicts()) {
-          solution.togglePiece(i, j);
-        } else {
-          // console.log(i, j)
-          // console.log(solution.rows())
-        }
+      solution.togglePiece(i, j)
+      if (solution.hasAnyRooksConflicts()) {
+        solution.togglePiece(i, j);
       }
     }
   }
-
   solution = solution.rows();
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0; //fixme
+
+  var board = new Board({n:n});
+
+  // Nested recursive function
+  var search = function(row) {  
+    // Base case
+    // When all rows are exhausted
+    if (row === n) {
+      // Increase solutionCount++
+      solutionCount++;
+      return;
+    }
+
+    // Loop through our rows
+    for (var i = 0; i < n; i++) {
+      // Toggle a piece on a column
+      board.togglePiece(row, i);
+      // If board has NO conflicts
+      if (!board.hasAnyRooksConflicts()) {
+        // Recurse through nested function -> send in columns
+        search(row + 1);
+      }
+      // Untoggle the piece on the column
+      board.togglePiece(row, i);
+    }
+  }
+  search(0);
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
@@ -50,7 +66,36 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  var board = new Board({n:n});
+  var solution = board.rows(); //fixme
+  
+  // Nested recursive function
+  var search = function(row) {  
+    // Base case
+    // When all rows are exhausted
+    if (row === n) {
+      
+      solution = _.map(board.rows(), function(row) {
+        return row.slice();
+      });
+      return;
+    }
+
+    // Loop through our rows
+    for (var i = 0; i < n; i++) {
+      // Toggle a piece on a column
+      board.togglePiece(row, i);
+      // If board has NO conflicts
+      if (!board.hasAnyQueensConflicts()) {
+        // Recurse through nested function -> send in columns
+        search(row + 1);
+      }
+
+      // Untoggle the piece on the column
+      board.togglePiece(row, i);
+    }
+  }
+  search(0);
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
@@ -58,7 +103,38 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  // var solutionCount = undefined; //fixme
+  var solutionCount = 0; //fixme
+  var board = new Board({n:n});
+  
+  // Nested recursive function
+  var search = function(row) {  
+    // Base case
+    // When all rows are exhausted
+    if (row === n) {
+      // Increase solutionCount++
+      solutionCount++;
+      return;
+    }
+
+    // Loop through our rows
+    for (var i = 0; i < n; i++) {
+      // Toggle a piece on a column
+      board.togglePiece(row, i);
+      // If board has NO conflicts
+      console.log(board);
+      if (!board.hasAnyQueensConflicts()) {
+        // Recurse through nested function -> send in columns
+        search(row + 1);
+      }
+
+      // Untoggle the piece on the column
+      board.togglePiece(row, i);
+    }
+  }
+  search(0);
+
+
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
